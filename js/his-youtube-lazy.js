9e896a207a19101393016df501e23c4cebfc6cd5
Dragos Plesca <plescadragoss@yahoo.com>
2013-02-09 01:47:25 +0200

    Added support for shortcode youtube lazy-load iframe

---
+++ js/his-youtube-lazy.js
@@ -0,0 +1,12 @@
+(function($){
+    $('.his-youtube-video img, .his-youtube-play svg').click(function(){
+        var video_container;
+        if( $(this).parent().attr('class') == 'his-youtube-video' ){
+            video_container = $(this).parent();             
+        } else{             
+            video_container = $(this).parent().parent();                
+        }
+        
+        video_container.html('<iframe width="640" height="360" src="http://www.youtube.com/embed/' + $(this).attr('data-guid') + '?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+    });
+})(jQuery);
