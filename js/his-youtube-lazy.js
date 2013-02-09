ec8c93db39f232aee5cc12ed4bdbc3f62e1ea381
Dragos Plesca <plescadragoss@yahoo.com>
2013-02-09 16:44:08 +0200

    make youtube iframe lazyload onready

--- js/his-youtube-lazy.js
+++ js/his-youtube-lazy.js
@@ -1,4 +1,4 @@
-(function($){
+jQuery(document).ready(function($) {
     $('.his-youtube-video img, .his-youtube-play svg').click(function(){
         var video_container;
         if( $(this).parent().attr('class') == 'his-youtube-video' ){
@@ -9,4 +9,4 @@
         
         video_container.html('<iframe width="640" height="360" src="http://www.youtube.com/embed/' + $(this).attr('data-guid') + '?autoplay=1" frameborder="0" allowfullscreen></iframe>');
     });
-})(jQuery);
+});
